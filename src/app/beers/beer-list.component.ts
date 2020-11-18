8039ea681b2461d16ce3b192495e48f0764a22bb
Michael McCulloch <mjmccull0@gmail.com>
2020-11-18 18:28:44 -0500

    Added persistent search query for beer-list
    Resolves: #5

--- src/app/beers/beer-list.component.ts
+++ src/app/beers/beer-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Beer } from './shared/beer';
 import { BeerService } from './shared/beer.service';
 
@@ -12,21 +13,56 @@ import { BeerService } from './shared/beer.service';
 export class BeerListComponent implements OnInit {
   title = "Angular Brew";
 
+  beers: Beer[];
+
   searchForm = new FormGroup({
     beer_name: new FormControl('')
   });
 
-  beers: Beer[];
+  constructor(
+    private beerService: BeerService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {
 
-  constructor(private beerService: BeerService) { }
+    this.route.queryParams.subscribe(params => {
+      this.searchForm.patchValue(params);
+    });
 
-  ngOnInit() {
+  }
+
+  ngOnInit(): void {
     this.getBeers();
   }
 
   getBeers(): void {
     this.beerService.getBeers(this.searchForm.value)
       .subscribe(beers => this.beers = beers);
+  }
+
+  clearBeerName(): void {
+    this.searchForm.patchValue({ beer_name: '' });
+    this.updateQuery();
+  }
+
+  onSubmit() {
+    this.updateQuery();
+  }
+
+  updateQuery() {
+    const activeParams = {};
+    const formValue = this.searchForm.value;
+
+    // Collect form values that are not empty. 
+    Object.keys(formValue).forEach(key => {
+      if (formValue[key] !== (undefined || null || "")) {
+        activeParams[key] = formValue[key];
+      }
+    });
+
+    // Construct a url with non-empty form values.
+    this.router.navigate([''], { queryParams: activeParams })
+      .then(_ => this.getBeers());
   }
 
 }
