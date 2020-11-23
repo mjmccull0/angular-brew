c3828c71161ccb6a7854c0d1a91ceac40f308d1e
Michael McCulloch <mjmccull0@gmail.com>
2020-11-23 09:41:14 -0500

    Improved related beer scroll controls
    Now the related beer controls scroll a number of beers based on viewport
    size.  Item size is now a required input, rather than being hard-coded.

--- src/app/beers/related-beer/related-beer.component.ts
+++ src/app/beers/related-beer/related-beer.component.ts
@@ -11,11 +11,16 @@ import { BeerService } from 'service/beer/beer.service';
 export class RelatedBeerComponent implements OnChanges {
   @ViewChild('viewport')
   viewport: CdkVirtualScrollViewport;
+
   scrollIndex = 0;
+  scrollDistance: number;
+  scrollLeftDisabled: boolean;
+  scrollRightDisabled: boolean;
 
   @Input() title: string;
   @Input() relation;
   @Input() beer: Beer;
+  @Input() itemSize: number;
 
   beers: Beer[];
 
@@ -39,18 +44,33 @@ export class RelatedBeerComponent implements OnChanges {
 
   onScroll(event): void {
     this.scrollIndex = event;
+    this.scrollDistance = Math.floor(this.viewport.getViewportSize() / this.itemSize);
+
+    if (this.viewport.getOffsetToRenderedContentStart() == 0) {
+      this.scrollLeftDisabled = true;
+    } else {
+      this.scrollLeftDisabled = false;
+    }
+
+    if (this.viewport.measureScrollOffset('right') < this.itemSize) {
+      this.scrollRightDisabled = true;
+    } else {
+      this.scrollRightDisabled = false;
+    }
+
   }
 
   scrollLeft(): void {
     if (this.scrollIndex > 0) {
-      this.viewport.scrollToIndex(--this.scrollIndex, "smooth");
+      const newScrollIndex = this.scrollIndex - this.scrollDistance;
+      this.viewport.scrollToIndex(newScrollIndex, "smooth");
     }
   }
 
   scrollRight(): void {
-    if (this.beers.length > this.scrollIndex) {
-      this.viewport.scrollToIndex(++this.scrollIndex, "smooth");
+    if (this.scrollIndex < this.beers.length) {
+      const newScrollIndex = this.scrollIndex + this.scrollDistance;
+      this.viewport.scrollToIndex(newScrollIndex, "smooth");
     }
   }
-
 }
